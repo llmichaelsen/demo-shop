a1540ea13f81ecb6c874dd793b00d6da54ee8f1e
Leonardo Lovatto <llmichaelsen@gmail.com>
2020-05-20 15:31:23 -0300

    Header

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ComponentsModule } from './components/components.module';
+import { FooterComponent } from './componets/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
